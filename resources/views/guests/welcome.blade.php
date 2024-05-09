3ac7e6e9a77075a41dc617f904beb628bebe5a4e
GioMench <giorgiaglm@gmail.com>
2024-05-09 19:16:18 +0200

    take info from database and print it on web

---
+++ resources/views/guests/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+   
+    @forelse ($movies as $movie )
+        <h2>
+            {{$movie['title']}}
+        </h2>
+    @empty
+        <p>sorry,movie not found!</p>
+        
+    @endforelse
+
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[PageController::class, 'index'] );
+Route::get('/about',[PageController::class, 'about'] );
+Route::get('/contacts',[PageController::class, 'contacts'] );
+
